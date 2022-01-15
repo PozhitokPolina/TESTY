bde142d3994bf88992cd67b37bbdecf6d4a87704
PozhitokPolina <polinka.pps@gmail.com>
2022-01-15 18:05:23 +0300

    commit game

---
+++ ship.py
@@ -0,0 +1,36 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Ship(Sprite):
+    def __init__(self, ai_game):
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.screen_rect = ai_game.screen.get_rect()
+
+        self.image = pygame.image.load('TESTY/ship.bmp')
+        self.rect = self.image.get_rect()
+        self.rect.midbottom = self.screen_rect.midbottom
+
+        self.x = float(self.rect.x)
+
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        if self.moving_right:
+            self.rect.x += 1
+            self.x += self.settings.ship_speed
+        if self.moving_left:
+            self.x -= self.settings.ship_speed
+            self.rect.x -= 1
+
+            self.rect.x = self.x
+
+    def blitme(self):
+
+        self.screen.blit(self.image, self.rect)
+
+    def center_ship(self):
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
